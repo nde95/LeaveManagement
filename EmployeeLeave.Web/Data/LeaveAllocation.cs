cb6044a7dea3b137e1c5d6a373abb22bdd5c9b9a
nde95 <ndennison268@gmail.com>
2023-06-14 12:03:08 -0400

    Added new DB migrations and tables

---
+++ EmployeeLeave.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeLeave.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+    }
+}
