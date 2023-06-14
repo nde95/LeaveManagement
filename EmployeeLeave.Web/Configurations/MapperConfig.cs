24c56d1c7689c4f57ae63a4823e84c9b79eaf705
nde95 <ndennison268@gmail.com>
2023-06-14 17:47:15 -0400

    configuring mapper for VM

---
+++ EmployeeLeave.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EmployeeLeave.Web.Data;
+using EmployeeLeave.Web.Models;
+
+namespace EmployeeLeave.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
