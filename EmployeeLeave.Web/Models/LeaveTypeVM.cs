1ac64786597f325e94495fce1a6df1b894912975
nde95 <ndennison268@gmail.com>
2023-06-15 12:34:29 -0400

    form submit validation

--- EmployeeLeave.Web/Models/LeaveTypeVM.cs
+++ EmployeeLeave.Web/Models/LeaveTypeVM.cs
@@ -7,9 +7,12 @@ namespace EmployeeLeave.Web.Models
         public int Id { get; set; }
 
         [Display(Name = "Leave Type")]
+        [Required]
         public string Name { get; set; }
 
         [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter A Valid Number")]
         public int DefaultDays { get; set; }
     }
 }
